5f50226a6d082a6c981441e695548029afc4d571
Tiago Amorim <dvalinn@localhost.localdomain>
2023-10-10 22:51:00 +0100

    udpated deliverable client

--- deliverable/files/clienteA.c
+++ deliverable/files/clienteA.c
@@ -24,6 +24,33 @@
 #define LOCALHOST "127.0.0.1"
 #define PORT 502
 
+uint16_t *toBigEndian16Bit(uint8_t *data, int len) {
+    uint16_t *result = (uint16_t *)malloc(len * sizeof(uint16_t) / 2);
+    for (int i = 0; i < len; i++) {
+        result[i] = data[i * 2] << 8 | data[i * 2 + 1];
+    }
+    return result;
+}
+
+int checkForExceptions(uint8_t *packet, int pLen, int expectedLen) {
+    if (packet == NULL) {
+        ERROR("checkForExceptions: packet is NULL\n");
+        return -1;
+    }
+
+    if (packet[0] & 0x80) {
+        ERROR("Exception: %d\n", packet[1]);
+        return packet[1];
+    }
+
+    if (pLen != expectedLen) {
+        ERROR("checkForExceptions: incorrect response lenght\n");
+        return -1;
+    }
+
+    return 0;
+}
+
 int main() {
     int socketfd = connectToServer(LOCALHOST, PORT);
     if (socketfd == -1) {
@@ -32,7 +59,7 @@ int main() {
         return -1;
     }
 
-    int rLen1, rLen2;
+    int rLen1, rLen2, error;
     uint8_t *response1, *response2;
     uint16_t startingAddress, quantity, transactionID;
 
@@ -40,26 +67,15 @@ int main() {
     quantity = 1;
     transactionID = 1;
 
-    // 1. write 0x41 to the 121th register
+    //* 1. write 0x41 to the 121th register
     uint16_t data1[] = {0x41};
 
     response1 = writeMultipleRegisters(socketfd, startingAddress, transactionID, quantity, data1, &rLen1);
-    if (response1 == NULL) {
-        ERROR("writeMultipleRegisters failed\n");
-        return -1;
-    }
-
-    // handle server exeption
-    if (response1[0] & 0x80) {
-        ERROR("writeMultipleRegisters failed\n");
-        printf("result = %d\n", response1[1]);
-        return response1[1];
-    }
-
-    // check if rLen1 is correct (should be 5 bytes - 1 for function code, 2 for starting address, 2 for quantity)
-    if (rLen1 != 5) {
-        ERROR("writeMultipleRegisters: incorrect response lenght\n");
-        printf("result = -1\n");
+    // expected response length: 5 bytes - 1 for function code, 2 for starting address, 2 for quantity
+    error = checkForExceptions(response1, rLen1, 5);
+    if (error != 0) {
+        ERROR("exit on %s, %d\n", __FILE__, __LINE__);
+        printf("result = %d\n", error);
         return -1;
     }
 
@@ -67,7 +83,7 @@ int main() {
 
     INFO("Part 1 done\n");
 
-    // 2. read values from registers 122-125
+    //* 2. read values from registers 122-125
 
     startingAddress = 121;
     quantity = 4;
@@ -75,68 +91,35 @@ int main() {
     transactionID = 2;
 
     response1 = readHoldingRegisters(socketfd, startingAddress, transactionID, quantity, &rLen1);
-    if (response1 == NULL) {
-        ERROR("Read Holding Registers failed\n");
-        printf("result = -1\n");
-        return -1;
-    }
-
-    // handle server exeption
-    if (response1[0] & 0x80) {
-        ERROR("Read Holding Registers exception\n");
-        printf("result = %d\n", response1[1]);
-        return response1[1];
-    }
-
-    // check if rLen1 is correct (should be 10 - 2 bytes for function code and byte count, 8 bytes for data)
-    if (rLen1 != 10) {
-        ERROR("Read Holding Registers: incorrect response lenght\n");
-        printf("result = -1\n");
-        return -1;
+    error = checkForExceptions(response1, rLen1, 10);
+    if (error != 0) {
+        ERROR("exit on %s, %d\n", __FILE__, __LINE__);
+        printf("result = %d\n", error);
+        return error;
     }
 
     INFO("Part 2 done\n");
 
-    // 3. read the value from register 126
+    //* 3. read the value from register 126
     startingAddress = 125;
     quantity = 1;
 
     transactionID = 3;
 
     response2 = readHoldingRegisters(socketfd, startingAddress, transactionID, quantity, &rLen2);
-    if (response2 == NULL) {
-        ERROR("readHoldingRegisters failed\n");
-        printf("result = -1\n");
-        return -1;
-    }
-
-    // handle server exeption
-    if (response2[0] & 0x80) {
-        ERROR("readHoldingRegisters exception\n");
-        printf("result = %d\n", response2[1]);
-        return response2[1];
-    }
-
-    // check if rLen2 is correct (should be 4 - 2 bytes for function code and byte count, 2 bytes for data)
-    if (rLen2 != 4) {
-        ERROR("readHoldingRegisters: incorrect response lenght\n");
-        printf("result = -1\n");
-        return -1;
+    error = checkForExceptions(response2, rLen2, 4);
+    if (error != 0) {
+        ERROR("exit on %s, %d\n", __FILE__, __LINE__);
+        printf("result = %d\n", error);
+        return error;
     }
 
     INFO("Part 3 done\n");
 
-    // 3.5 calculate C
+    //* 3.5 calculate C
     uint8_t *responseData = &response1[2];
-
-    uint16_t A[4] = {
-        responseData[0] << 8 | responseData[1],
-        responseData[2] << 8 | responseData[3],
-        responseData[4] << 8 | responseData[5],
-        responseData[6] << 8 | responseData[7]};
-
+    uint16_t *A = toBigEndian16Bit(responseData, 8);
     uint16_t B = response2[2] << 8 | response2[3];
-
     uint16_t C = 0;
 
     if (B == 0) {
@@ -145,6 +128,7 @@ int main() {
         C = A[0] + A[3];
     }
 
+    free(A);
     free(response1);
     free(response2);
 
@@ -153,7 +137,7 @@ int main() {
     INFO("B = %04X\n", B);
     INFO("C = %04X\n", C);
 
-    // 4. write C to register 127 LOCALHOST
+    //* 4. write C to register 127 LOCALHOST
     startingAddress = 126;
     quantity = 1;
 
@@ -162,24 +146,11 @@ int main() {
     uint16_t data2[] = {C};
 
     response1 = writeMultipleRegisters(socketfd, startingAddress, transactionID, quantity, data2, &rLen1);
-    if (response1 == NULL) {
-        ERROR("writeMultipleRegisters failed\n");
-        printf("result = -1\n");
-        return -1;
-    }
-
-    // handle server exeption
-    if (response1[0] & 0x80) {
-        ERROR("writeMultipleRegisters exception\n");
-        printf("result = %d\n", response1[1]);
-        return response1[1];
-    }
-
-    // check if rLen1 is correct (should be 5 bytes - 1 for function code, 2 for starting address, 2 for quantity)
-    if (rLen1 != 5) {
-        ERROR("writeMultipleRegisters: incorrect response lenght\n");
-        printf("result = -1\n");
-        return -1;
+    error = checkForExceptions(response1, rLen1, 5);
+    if (error != 0) {
+        ERROR("exit on %s, %d\n", __FILE__, __LINE__);
+        printf("result = %d\n", error);
+        return error;
     }
 
     free(response1);
@@ -189,7 +160,7 @@ int main() {
     INFO("Part 4 done\n");
 
 #pragma region Part 5
-    // 5. Write C to register 128 REMOTEHOST
+    //* 5. Write C to register 128 REMOTEHOST
 
     socketfd = connectToServer(REMOTEHOST, PORT);
     if (socketfd == -1) {
@@ -204,30 +175,16 @@ int main() {
     transactionID = 5;
 
     response1 = writeMultipleRegisters(socketfd, startingAddress, transactionID, quantity, data2, &rLen1);
-    if (response1 == NULL) {
-        ERROR("writeMultipleRegisters failed\n");
-        printf("result = -1\n");
-        return -1;
-    }
-
-    // handle server exeption
-    if (response1[0] & 0x80) {
-        ERROR("writeMultipleRegisters exception\n");
-        printf("result = %d\n", response1[1]);
-        return response1[1];
-    }
-
-    // check if rLen1 is correct (should be 5 bytes - 1 for function code, 2 for starting address, 2 for quantity)
-    if (rLen1 != 5) {
-        ERROR("writeMultipleRegisters: incorrect response lenght\n");
-        printf("result = -1\n");
-        return -1;
+    error = checkForExceptions(response1, rLen1, 5);
+    if (error != 0) {
+        ERROR("exit on %s, %d\n", __FILE__, __LINE__);
+        printf("result = %d\n", error);
+        return error;
     }
 
     free(response1);
-
     disconnectFromServer(socketfd);
-    
+
 #pragma endregion  // Part 5
 
     printf("result = 0\n");
